6e8e9872f137b0d5f49271fff8fe2efde4b22b53
addi009 <addi.ahmad9@gmail.com>
2023-05-11 02:53:52 +0500

    update

--- routes/website/_website.php
+++ routes/website/_website.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Website\ContactUsController;
 use App\Http\Controllers\Website\HomeController;
 use App\Http\Controllers\Website\NannyController;
 use App\Http\Controllers\Website\PricingController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,9 @@ Route::get('/all-nannies', [NannyController::class,'index'])->name('all-nannies'
 Route::get('/nanny/profile/{id}', [NannyController::class,'profile'])->name('nanny.profile');
 Route::get('/pricing', [PricingController::class,'index'])->name('pricing');
 Route::get('/contact-us', [ContactUsController::class,'index'])->name('contact-us');
+
+
+Route::get('/run', function () {
+    Artisan::call("migrate");
+    Artisan::call("db:seed");
+});
